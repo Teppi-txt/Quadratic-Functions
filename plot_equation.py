f6440755d04e32fadb8b5b0b9038f58f10d451d8
Teppi Le <64125245+Teppi-txt@users.noreply.github.com>
2020-09-13 22:11:41 +0700

    Add files via upload

---
+++ plot_equation.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import sympy
+import numpy as np
+
+fig = plt.figure()
+axis = fig.add_subplot(1, 1, 1)
+
+axis.spines['left'].set_position('center'); axis.spines['bottom'].set_position('zero')
+axis.spines['right'].set_color('none'); axis.spines['top'].set_color('none')
+axis.xaxis.set_ticks_position('bottom'); axis.yaxis.set_ticks_position('left')
+
+def plot_quad(y_expr):
+    x = np.linspace(-100,100,100)
+    y = x**2 + 5*x + 6
+    plt.plot(x,y, 'r')
+    plt.show()
