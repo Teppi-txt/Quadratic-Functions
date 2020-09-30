f2f9ad95a6ebaa717c0b1ba9381f769e5e896ffd
Teppi Le <64125245+Teppi-txt@users.noreply.github.com>
2020-09-30 20:01:09 +0700

    Update equation_functions.py

--- equation_functions.py
+++ equation_functions.py
@@ -1,10 +1,15 @@
 import matplotlib
 import sympy
 
+#factorization function using sympy.
 def factorize(string_eq):
+    #converting stringed equation to equation.
     equation = sympy.parse_expr(string_eq)
     return sympy.factor(equation)
 
+#expansion method.
 def expansion(string_eq):
+    #converting stringed equation to equation.
     equation = sympy.parse_expr(string_eq)
+    #returns expanded equation
     return sympy.expand(equation)
